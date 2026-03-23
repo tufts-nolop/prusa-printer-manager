eeb99ba129edf36ef729a19e834e77ef07957a74
Daniel Opara <58013370+bimmui@users.noreply.github.com>
2026-03-23 12:39:00 -0400

    Remove DEBUG_PROPAGATE_EXCEPTIONS setting
    Removed DEBUG_PROPAGATE_EXCEPTIONS setting for production.

--- prusa_print_client/prusa_print_client/settings.py
+++ prusa_print_client/prusa_print_client/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = [h.strip() for h in os.getenv("ALLOWED_HOSTS", "").split(",") if
 
 if DEBUG and not ALLOWED_HOSTS:
     ALLOWED_HOSTS = ["*"]  # dev convenience
-DEBUG_PROPAGATE_EXCEPTIONS = True
 
 
 # Application definition
